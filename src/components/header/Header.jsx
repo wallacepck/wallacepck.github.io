9e3535c05dc3bfab0a8c6f810056dc2e63da2505
Darren-Tung <darrentung01@gmail.com>
2023-12-15 21:03:26 +0800

    v0.1.2
    29.30min

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,16 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./header.css";
 
 const Header = () => {
+    const[Toggle, showMenu] = useState(false);
   return (
     <header className='header'>
         <nav className="nav container">
             <a href="index.html" className="nav__logo">Darren</a>
 
-            <div className="nav__meu">
+            <div className="nav__menu">
                 <ul className="nav__list grid">
                     <li className="nav__item">
-                        <a href="#home" className="nav__link">
+                        <a href="#home" className="nav__link active-link">
                             <i className="uil uil-estate nav__icon"></i> Home
                         </a>
                     </li>
