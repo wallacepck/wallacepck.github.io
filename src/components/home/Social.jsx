be52e1b5952cf865aadba574c57499dfde9bd4f1
Darren-Tung <darrentung01@gmail.com>
2023-12-16 15:59:51 +0800

    v0.2.2
    Deployable on Github, up to 45min

---
+++ src/components/home/Social.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Social = () => {
+  return (
+    <div className="home__social">
+        <a href="https://t.me/dat_yanzuo" className="home__social-icon" target="_blank">
+            <i class="fa-brands fa-telegram"></i>
+        </a>
+
+        <a href="https:linkedin.com/in/darren-tung-271113168/" className="home__social-icon" target="_blank">
+            <i class="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="https:discordapp.com/users/222725107309608960" className="home__social-icon" target="_blank">
+            <i class="fa-brands fa-discord"></i>
+        </a>
+
+        <a href="https://www.instagram.com/dat_yanzuo/" className="home__social-icon" target="_blank">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </div>
+  )
+}
+
+export default Social
