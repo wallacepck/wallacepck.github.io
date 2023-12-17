1323591c9bb10c346fe218323a25e4c4784a32c1
Darren-Tung <darrentung01@gmail.com>
2023-12-17 11:42:36 +0800

    v0.5.1
    Skills done

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+
+const Backend = () => {
+  return (
+    <div className="skills__content">
+        <h3 className="skills__title">Frameworks & Resources</h3>
+
+        <div className="skills__box">
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Arduino Studio</h3>
+                        <span className="skills__level">Intermediate</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">NodeJS</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Computer Vison</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">R</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+
+            <div className="skills__group">
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Google App Script</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Unity</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Firebase</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+
+                <div className="skills__data">
+                    <i class='bx bx-badge-check'></i>
+                    <div>
+                        <h3 className="skills__name">Git</h3>
+                        <span className="skills__level">Basic</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Backend
