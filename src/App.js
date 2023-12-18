d9ec7e7942adfe7f235f92689de183876d63032d
Darren-Tung <darrentung01@gmail.com>
2023-12-18 20:48:41 +0800

    v0.7.1

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <About />
       <Skills />
       <Qualification />
+      <Contact />
     </main>
     </>
   );
