f5fa28211aeadb1633f77482904929226e689171
Darren-Tung <darrentung01@gmail.com>
2023-12-26 13:53:23 +0800

    v0.8.1
    footer done

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import About from './components/about/About';
 import Skills from './components/skills/Skills';
 import Qualification from './components/qualification/Qualification';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
       <Skills />
       <Qualification />
       <Contact />
+      <Footer />
     </main>
     </>
   );
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./footer.css"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+        <div className="footer__container container">
+            <h1 className="footer__title">Darren Tung</h1>
+
+            <ul className="footer__list">
+                <li>
+                    <a href="#about" className="footer__link">About</a>
+                </li>
+
+                <li>
+                    <a href="#portfolio" className="footer__link">Projects</a>
+                </li>
+            </ul>
+
+            <span className="footer__copy">
+                &#169; DarrenTung. All rights reserved
+            </span>
+        </div>
+    </footer>
+  )
+}
+
+export default Footer
