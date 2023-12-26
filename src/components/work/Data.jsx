bbc902a6827470bab2e0cfcfc22d2b359517729a
Darren-Tung <darrentung01@gmail.com>
2023-12-26 18:44:25 +0800

    v1.0.0
    Deployable

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,39 +1,64 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/ParadeStateProj.jpg";
+import Work2 from "../../assets/dsta.png";
+import Work3 from "../../assets/LegoProject.png";
+import Work4 from "../../assets/FYP_robot1.jpg";
+import Work5 from "../../assets/Rnd_project1.jpg";
+import Work6 from "../../assets/NRC_Robot.jpg";
 
 export const projectsData = [
     {
       id: 1,
       image: Work1,
-      title: "Web design",
-      category: "web",
+      title: "Parade State Project",
+      category: "Non-school",
+      backgroundTitle: "A project from Jan 2022.",
+      background: "The system in MINDEF can be very inefficent, hence created created 1 Microsoft Excel macro project using Microsoft VBA and Created 2 Telegram Bots using Google App Script | @BMTC_Athena_Bot & @Athena_MC_Bot",
+      link: "https://drive.google.com/file/d/18pl2Rn5UUkmvBA5IxlcUsSUNbdRFXsTK/view?usp=sharing",
     },
     {
       id: 2,
       image: Work2,
-      title: "App movil",
-      category: "app",
+      title: "AI Chatbot & MLOps",
+      category: "Non-School",
+      backgroundTitle: "Projects from May 2021 at DSTA.",
+      background: "Worked on an AI chatbot including Rasa Open Source, and worked on a project on Harbor, Docker and MLOps",
+      link: "https://rasa.com/docs/rasa/",
     },
     {
       id: 3,
       image: Work3,
-      title: "Brand design",
-      category: "design",
+      title: "Lego Brick Detection",
+      category: "School",
+      backgroundTitle: "A project from Jan 2021.",
+      background: "It detects and counts different types of Lego bricks using OpenCV",
+      link: "https://drive.google.com/file/d/1wbu8WojZS0Mn_pDxRTtFRHj2SroiPaLu/view?usp=sharing",
     },
     {
       id: 4,
       image: Work4,
-      title: "App movil",
-      category: "app",
+      title: "Autonomous Security Surveillance",
+      category: "School",
+      backgroundTitle: "Polytechnic FYP from Aug 2020.",
+      background: "This project was preceded by multiple batches of seniors. Changed the framework of the code to support WebRTC using NodeJS to improve connection latency significantly, and improved communication & movement capabilities using Arduino, Tinkercad, ROS & servo motors",
+      link: "https://drive.google.com/file/d/17zSOAsQ2pmVJUwayGophEfGVmdz96ogB/view?usp=sharing",
     },
     {
       id: 5,
       image: Work5,
-      title: "Web design",
-      category: "web",
+      title: "Mental Health Optimisation",
+      category: "School",
+      backgroundTitle: "A R&D Project from Feb 2020.",
+      background: "Worked with Dr Harry Nguyen, AI Professor at SIT, and Dr Øystein Eiring, Psychologist from Norway. Used the analysis of medical disorders and study of gamification to ideate a medical app which greatly increases the habit of adherence. Used Adobe XD & Unity Game Engine to design and create the app",
+      link: "https://drive.google.com/file/d/1JX8zxDpHRwfjiBLNBnhXCaZ0xRf7EYaS/view?usp=sharing",
+    },
+    {
+      id: 6,
+      image: Work6,
+      title: "National Robotic Competition",
+      category: "Non-school",
+      backgroundTitle: "Participated in 2018 and 2019.",
+      background: "Every year participants are given a challenge project to achieve using robotics",
+      link: "https://drive.google.com/file/d/17zSOAsQ2pmVJUwayGophEfGVmdz96ogB/view?usp=sharing",
     },
 ];
 
@@ -42,12 +67,9 @@ export const projectsNav = [
     name: 'all',
   },
   {
-    name: 'web',
+    name: 'school',
   },
   {
-    name: 'app',
-  },
-  {
-    name: 'design',
+    name: 'non-school',
   },
 ];
--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -5,8 +5,10 @@ const WorkItems = ({item}) => {
     <div className="work__card" key={item.id}>
         <img src={item.image} alt="" className='work__img'/>
         <h3 className="work__title">{item.title}</h3>
-        <a href="#" className="work__button">
-            Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
+        <h4 className="work__backgroundTitle">{item.backgroundTitle}</h4>
+        <h4 className="work__background">{item.background}</h4>
+        <a href={item.link} className="work__button" target="_blank">
+            More Details <i className="bx bx-right-arrow-alt work__button-icon"></i>
         </a>
     </div>
   )
