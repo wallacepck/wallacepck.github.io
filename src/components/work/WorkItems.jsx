acc22e24a29aeccef0390de500657f8baa73c664
Darren-Tung <40617230+Darren-Tung@users.noreply.github.com>
2024-07-31 15:56:06 +0800

    changed stuff

--- src/components/work/WorkItems.jsx
+++ src/components/work/WorkItems.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './work.css';
 
 const WorkItems = ({item}) => {
   return (
@@ -6,6 +7,7 @@ const WorkItems = ({item}) => {
         <img src={item.image} alt="" className='work__img'/>
         <h3 className="work__title">{item.title}</h3>
         <h4 className="work__backgroundTitle">{item.backgroundTitle}</h4>
+        <h4 className="work__backgroundDate">Date: {item.backgroundDate}<br></br>Software: {item.backgroundTech}</h4>
         <h4 className="work__background">{item.background}</h4>
         <a href={item.link} className="work__button" target="_blank">
             More Details <i className="bx bx-right-arrow-alt work__button-icon"></i>
